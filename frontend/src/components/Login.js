039c2b4d48686f95b39bfb17d63c7420f8d934c2
Saikou2 <152991302+Saikou2@users.noreply.github.com>
2024-10-03 01:45:08 +0000

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,16 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';  // Importez useNavigate
-import '../components/style.css';  // Importez le fichier CSS
-
+import { Link, useNavigate } from 'react-router-dom';  
+import '../components/style.css';  
 const Login = ({ setAuth }) => {
   const [credential, setCredential] = useState({
     username: 'saikou',
     password: 'saikou1234'
   });
   
-  const navigate = useNavigate();  // Créez une instance de navigate
-
+  const navigate = useNavigate();  
   const onChange = (e) => {
     setCredential({ ...credential, [e.target.name]: e.target.value });
   };
@@ -20,23 +18,22 @@ const Login = ({ setAuth }) => {
     
     try {
       const res = await axios.post('http://127.0.0.1:8000/api/custom_auth/login/', credential);
-      console.log('Réponse complète:', res); // Vérifie la structure de la réponse
-
-      // Supposons que le token se trouve dans res.data.access
+      console.log('Réponse complète:', res); 
+     
       const accessToken = res.data.access; 
       const superuser = res.data.is_superuser
       if (accessToken || superuser) {
-        localStorage.setItem('token', accessToken); // Stocke le token dans le localStorage
-        localStorage.setItem('superuser', superuser); // Stocke le token dans le localStorage
-        console.log('Token stocké:', accessToken); // Vérifie que le token est bien stocké
-        setAuth(true);  // Met à jour l'état d'authentification
-        navigate('/home');  // Redirige vers la page d'accueil
+        localStorage.setItem('token', accessToken); 
+        localStorage.setItem('superuser', superuser);
+        console.log('Token stocké:', accessToken); 
+        setAuth(true);  
+        navigate('/home');  
       } else {
         console.log('Token d\'accès introuvable dans la réponse');
       }
     } catch (err) {
       console.error('Erreur lors de la connexion:', err);
-      // Vous pouvez afficher un message d'erreur à l'utilisateur ici
+     
     }
   };
 
@@ -66,7 +63,7 @@ const Login = ({ setAuth }) => {
             <label>Password</label>
           </div>
           <div className="forgot-password">
-            {/* <a href="#">Forgot Password?</a> */}
+           
           </div>
           <button type="submit" className="btn">Login</button>
           <div className="signup-link">
